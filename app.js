7ab5b186375bc2f8810bf3f51100ceba1567e192
MarvelTimeline <connorhoskin12345@gmail.com>
2026-08-18 10:35:02 +0100

    Add basic order card click handling

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-
+document.querySelectorAll(".order-card").forEach(card => {
+    card.addEventListener("click", () => {
+        const order = card.dataset.order;
+        console.log("Selected order:", order);
+    });
+});
